8594997135514ad9b81c2762456477a4dfe85604
Abuzaid-01 <abuzaid0205@gmail.com>
2024-10-19 21:41:38 +0530

    sign up page

---
+++ app.js
@@ -0,0 +1,15 @@
+// document.getElementById("loginForm").addEventListener("submit", function(event) {
+//     event.preventDefault(); // Prevent form submission
+
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+//     var errorMessage = document.getElementById("errorMessage");
+
+//     // Simple validation for demonstration purposes
+//     if (username === "admin" && password === "password123") {
+//         alert("Login successful!");
+//         errorMessage.textContent = ""; // Clear error message
+//     } else {
+//         errorMessage.textContent = "Invalid username or password";
+//     }
+// });
